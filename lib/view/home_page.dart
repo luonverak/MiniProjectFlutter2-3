299f48680c78282ef424853f60dcc1847ff2ac25
luonverak <112792376+luonverak@users.noreply.github.com>
2024-02-07 15:28:02 +0700

    code commit

---
+++ lib/view/home_page.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:whatsapp_camera/whatsapp_camera.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  final files = ValueNotifier(<File>[]);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: ListView(
+          children: [
+            Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.sizeOf(context).width / 1.3,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey, width: 2),
+                  ),
+                  alignment: Alignment.center,
+                  child: const ListTile(
+                    leading: Icon(Icons.search),
+                    title: Text("What are you looking for?"),
+                  ),
+                ),
+                const SizedBox(width: 6),
+                GestureDetector(
+                  onTap: () async {
+                    List<File>? res = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WhatsappCamera(),
+                      ),
+                    );
+                    if (res != null) files.value = res;
+                  },
+                  child: const Icon(
+                    Icons.camera_alt,
+                    size: 28,
+                  ),
+                ),
+                const SizedBox(width: 7),
+                const badges.Badge(
+                  badgeContent: Text(
+                    '3',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  child: Icon(Icons.shopping_cart, size: 28),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:project1/view/cart_screen.dart';
+import 'package:project1/view/home_page.dart';
+import 'package:project1/view/profile_screen.dart';
+import 'package:project1/view/search_screen.dart';
+import 'package:project1/view/wishlist_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: listScreen[index],
+      ),
+      bottomNavigationBar: BottomBarFloating(
+        items: items,
+        backgroundColor: Colors.white,
+        color: Colors.black,
+        colorSelected: Colors.red,
+        indexSelected: index,
+        paddingVertical: 24,
+        onTap: (int index) => setState(() {
+          this.index = index;
+        }),
+      ),
+    );
+  }
+
+  int index = 0;
+
+  final List listScreen = [
+    HomePage(),
+    SearchScreen(),
+    WishListScreen(),
+    CartScreen(),
+    ProfileScreen()
+  ];
+  final List<TabItem> items = const [
+    TabItem(
+      icon: Icons.home_outlined,
+      title: 'Home',
+    ),
+    TabItem(
+      icon: Icons.notifications,
+      title: 'Notification',
+    ),
+    TabItem(
+      icon: Icons.favorite_border,
+      title: 'Wishlist',
+    ),
+    TabItem(
+      icon: Icons.shopping_cart_outlined,
+      title: 'Cart',
+    ),
+    TabItem(
+      icon: Icons.account_box,
+      title: 'profile',
+    ),
+  ];
+}
